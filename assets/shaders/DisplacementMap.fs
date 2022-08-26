3528cb56823482998db09c7c37012ff0d0702f8e
JonathanAckerman <jonathan@pragmapack.com>
2022-08-25 18:24:50 -0700

    Rename distortion shader to displacement

--- assets/shaders/DisplacementMap.fs
+++ assets/shaders/DisplacementMap.fs
@@ -3,18 +3,18 @@
 in vec2 fragTexCoord;
 
 uniform sampler2D waterSampler;
-uniform sampler2D distortionSampler;
-uniform vec2 distortionOffset;
+uniform sampler2D displacementSampler;
+uniform vec2 displacementOffset;
 
 out vec4 finalColor;
 
 void main()
 {
-    vec4 distortionTexel = texture(distortionSampler, fragTexCoord + distortionOffset/800);
-    float grayValue = distortionTexel.x / 255.0; // assumes r=g=b
+    vec4 displacementTexel = texture(displacementSampler, fragTexCoord + displacementOffset/800);
+    float grayValue = displacementTexel.x / 255.0; // assumes r=g=b
     float scale = 20.0;
     float offsetScalar = (2 * grayValue - 1) * scale;
     vec2 offset = vec2(20.0, 20.0) + vec2(offsetScalar, offsetScalar);
-    vec2 directionOffset = vec2(distortionOffset.x/600, -distortionOffset.y/600);
+    vec2 directionOffset = vec2(displacementOffset.x/600, -displacementOffset.y/600);
     finalColor = texture(waterSampler, fragTexCoord + directionOffset + offset);
 }
